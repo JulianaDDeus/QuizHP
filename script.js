fe9bf9761b173b4f20e12054de623851fc59dbc2
Juliana De Deus <juliana.ndeus@gmail.com>
2025-09-25 22:43:21 -0300

    resultados

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ window.onload = function() {
     var modal = new bootstrap.Modal(document.getElementById('AlertaInicio'));
     modal.show();
 }
-var perguntas = ["Quantos livros da saga Harry Potter existem?", 
+const perguntas = ["Quantos livros da saga Harry Potter existem?", 
                  "Quando foi lançado o primeiro livro?", 
                  "Quais são os nomes dos pais do Harry?", 
                  "Qual o nome da coruja do Harry?", 
                  "Qual desses personagens não é da Sonserina?", 
                  "Qual destas não é uma relíquia dos fundadores?", 
                  "Qual destas não é uma horcrux?",
-                 "Qual destas escolas não participou do torneio tribuxo?",
+                 "Qual destas escolas não participou do torneio tribruxo?",
                  "Em que filme Luna Lovegood aparece?",
                  "Qual é o nome do cão de três cabeças que protege o alçapão em Pedra Filosofal?"];
 
-var alternativas = [["8", "10", "7", "5"],
+const alternativas = [["8", "10", "7", "5"],
                     ["2010", "2002", "1985", "1997"],
                     ["Joane e Daniel Potter", "Lily e James Potter", 
                      "Angelina e Brad Potter", "Zendaya e Tom Potter"],
@@ -27,7 +27,7 @@ var alternativas = [["8", "10", "7", "5"],
                     ["Ordem da Fénix", "Pedra Filosofal", "Cálice de Fogo", "Câmara Secreta"],
                     ["Princesa", "Hercules", "Fofo", "Brutus"]];
 
-var respostas = ["7", "1997", "Lily e James Potter", "Edwiges", "Peter Pettigrew", 
+const respostas = ["7", "1997", "Lily e James Potter", "Edwiges", "Peter Pettigrew", 
                  "O anel de Slytherin", "Delphine Lestrange", "Clodtoer", "Ordem da Fénix", "Fofo"];
         
 let container = document.getElementById("quiz");
@@ -57,6 +57,7 @@ function MostrarPergunta(num) {
         var botao = document.createElement("button");
         botao.classList.add("btn", "btn-primary", "btn-block");
         botao.id = alternativas[num][i]
+        botao.type = "button";
         botao.onclick = function() {
             Analisar(alternativas[num][i], num);
         };
@@ -78,6 +79,25 @@ function Analisar(resposta, num){
     {
         MostrarPergunta(num);
     }else{
-        document.write(acertos);
+        Respostas(acertos);
     }
+}
+let titulo = document.getElementById("titulo");
+let texto = document.getElementById("resultado");
+function Respostas(acertos){
+    if(acertos <= 3){
+        titulo.innerHTML = "Só " + acertos + "/10?";
+        texto.innerHTML = "Alguém não prestou muita atenção nos filmes, não é mesmo? 😂 Mas tudo bem, mais sorte na próxima!"
+    }else if(acertos > 3 && acertos <= 6){
+        titulo.innerHTML = "" + acertos + "/10";
+        texto.innerHTML = "Foi bom, mas poderia ser melhor. Me parece uma boa desculpa para reassitir os filmes 👀"
+    }else if(acertos > 6 && acertos <= 9){
+        titulo.innerHTML = "" + acertos + "/10!";
+        texto.innerHTML = "Você foi muito bem! Quase acertou todas!😁"
+    }else{
+        titulo.innerHTML = "Wow!" + acertos + "/10!!!🎉";
+        texto.innerHTML = "Parabéns, você gabaritou o nosso teste! Você sabe muito sobre o universo Haary Potter!"
+    }
+    modal = new bootstrap.Modal(document.getElementById('resposta'));
+    modal.show();
 }
